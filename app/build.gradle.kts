63d342a2c0e3c4a19eb64b81f6b62c5eeaf5b281
HFDLYS <845388925@qq.com>
2025-01-22 08:25:31 +0800

    fix: 不知道是怎么回事

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
