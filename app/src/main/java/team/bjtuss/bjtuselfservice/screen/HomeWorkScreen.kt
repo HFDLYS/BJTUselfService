e782ce506564d7bedbbc18c1d3d33b9227ffaf0f
HFDLYS <845388925@qq.com>
2025-01-24 07:38:07 +0800

    feat: 随手写一下作业界面

--- app/src/main/java/team/bjtuss/bjtuselfservice/screen/HomeWorkScreen.kt
+++ app/src/main/java/team/bjtuss/bjtuselfservice/screen/HomeWorkScreen.kt
@@ -1,9 +1,28 @@
 package team.bjtuss.bjtuselfservice.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,6 +32,14 @@ import androidx.room.util.TableInfo
 import team.bjtuss.bjtuselfservice.repository.SmartCurriculumPlatformRepository
 import team.bjtuss.bjtuselfservice.viewmodel.MainViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import team.bjtuss.bjtuselfservice.entity.HomeworkEntity
 
 @Composable
 fun HomeWorkScreen(mainViewModel: MainViewModel) {
@@ -20,17 +47,178 @@ fun HomeWorkScreen(mainViewModel: MainViewModel) {
         mainViewModel.homeworkViewModel.syncDataAndClearChange()
     }
     val homeworkList by mainViewModel.homeworkViewModel.homeworkList.collectAsState()
-    LazyColumn {
-        items(homeworkList.size) {
-            val item = homeworkList[it]
-            Card {
-                Column {
-                    Text(text = item.title)
-                    Text(text = item.courseName)
-                    Text(text = item.endTime)
-                    // Text(text = item.content)
-                    Text(text = item.createDate)
+
+    HomeworkList(homeworkList)
+}
+
+@Composable
+fun HomeworkList(homeworkList: List<HomeworkEntity>) {
+    var filterExpanded by remember { mutableStateOf(false) }
+    var selectedFilter by remember { mutableStateOf("全部") }
+    var sortOrder by remember { mutableStateOf(SortOrder.DESCENDING) }
+
+    var filteredList = homeworkList
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp),
+        ) {
+            HomeworkSummaryCard(filteredList)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(onClick = { filterExpanded = true }) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "Filter Options"
+                    )
                 }
+
+                DropdownMenu(
+                    expanded = filterExpanded,
+                    onDismissRequest = { filterExpanded = false }
+                ) {
+                    val filterOptions = mutableListOf("全部")
+                    filterOptions.addAll(homeworkList.map { it.courseName }.distinct())
+                    filterOptions.forEach { option ->
+                        DropdownMenuItem(
+                            onClick = {
+                                selectedFilter = option
+                                filteredList = if (option == "全部") {
+                                    homeworkList
+                                } else {
+                                    homeworkList.filter { it.courseName == option }
+                                }
+                                filterExpanded = false
+                            },
+                            text = {
+                                Text(
+                                    text = option,
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = MaterialTheme.colorScheme.onSurface
+                                )
+                            }
+                        )
+                    }
+                }
+            }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(bottom = 16.dp)
+            ) {
+                items(filteredList.size) { index ->
+                    val homework = filteredList[index]
+                    HomeworkItemCard(homework)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeworkSummaryCard(homeworkList: List<HomeworkEntity>) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "作业安排：${homeworkList.size}项",
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeworkItemCard(homework: HomeworkEntity) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable { },
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = homework.title,
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
+                ),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = homework.courseName,
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
+                ),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Schedule,
+                    contentDescription = "Create Date",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(16.dp),
+                )
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Text(
+                        text = "发起时间: ${homework.createDate}",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Text(
+                        text = "截止时间: ${homework.endTime}",
+                        style = MaterialTheme.typography.bodyMedium.copy(
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    )
+                }
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Info,
+                    contentDescription = "Status",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(16.dp),
+                )
+                Text(
+                    text = "状态: ${homework.submitCount}/${homework.allCount}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
         }
     }
